ca6e910de8f421a5999254e5a41d7740d31444fc
GAAfan15 <16Amcgee@stjosephsrush.com>
2018-05-17 09:43:10 +0100

    Create Unfinished start.py

---
+++ Unfinished start.py
@@ -0,0 +1,39 @@
+from time import sleep
+print("Hello and welcome to our CBA")
+sleep(2)
+print("This is an educational History game made by Clare, Nancy ,Dylan and Aaron")
+sleep(5)
+print("Welcome to the year 2000")
+sleep(1)
+print("*You see a old looking car and decide to steal it*")
+sleep(1)
+print("*You suddenly regret it as a police car darts towards you*")
+sleep(1)
+print("*You hit the accelorator as the car darts off suprsing fast")
+sleep(1)
+print("You realise you are in a Delorean!")
+sleep(1)
+print("*The speedometer goes over 88 and the car begins to spark and disspiears from the police chase*")
+sleep(1)
+print("You rember that your names is Gavin for no reason")
+sleep(1)
+print("*The car stops as it suddenly hits a tree*")
+sleep(1)
+print("You are outside King Henry the eights palice
+sleep(1)
+print("Hampton Court Palace looks supprisingly small from here")
+sleep(1)
+print("Two knights run towards yo!!!")
+sleep(1)
+print("What do you do??")
+
+sleep(1)
+
+choice=input("Press 1 to punch them or press 2 to offer them tea")
+      if choice==1:
+      print("They easily chop your head clean off")
+
+
+      elif choice==2:
+      print(" They Curious about what this tea is?")
+      

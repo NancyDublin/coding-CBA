13844a7e57a5853431a8ba0d9c97edefe9e46db7
GAAfan15 <16Amcgee@stjosephsrush.com>
2018-05-28 13:48:01 +0100

    Add files via upload

---
+++ Pythoncodegamecba.py
@@ -0,0 +1,92 @@
+from time import sleep
+#introduction
+print("Hello and welcome to our CBA")
+sleep(2)
+print("This is an educational History game made by Clare, Nancy ,Dylan and Aaron")
+sleep(5)
+print("Welcome to the year 2000")
+sleep(1)
+print("*You see a old looking car and decide to steal it*")
+sleep(1)
+print("*You suddenly regret it as a police car darts towards you*")
+sleep(1)
+print("*You hit the accelorator as the car darts off suprsing fast")
+sleep(1)
+print("You realise you are in a Delorean!")
+sleep(1)
+print("*The speedometer goes over 88 and the car begins to spark and disspiears from the police chase*")
+sleep(1)
+print("You rember that your names is Gavin for no reason")
+sleep(1)
+print("*The car stops as it suddenly hits a tree*")
+sleep(1)
+#introduction
+
+#France
+print("You get out of the car to find yourself in France")
+sleep(1)
+print("There is no Eiffle tower and the location looks very run")
+sleep(1)
+print("*The car beeps as a time appears on the screen on the steering wheel*")
+sleep(1)
+print("July 14,1789 aka Bastile day!")
+sleep(2)
+print("You watch as an angry gang of 3 estate french people march to a large building")
+sleep(3)
+print("This was when the day the 3rd estate attacked the prison for weapons to use in a revolution")
+sleep(5)
+
+#The names and power of each of the estates
+print("The first estate was the Clergy aka the religious figures")
+sleep(1)
+print("The second where the nobility")
+sleep(1)
+print("The third where everyone else or the commoners")
+sleep(1)
+
+
+
+
+
+
+
+
+#Qestion time
+print("Question time!")
+
+print("What day is Bastile day??")
+
+choice=input("Press 1 for July 12th 1789 and press 2 for July 14,1789 ")#Telling it to do a task
+if choice==1:#Reacting to the choice you made
+    print("Wrong")
+
+elif choice==2:
+    print("Correct")
+
+
+
+
+
+
+print("You get into the car as it brings you to a court room in France")
+sleep(1)
+print("The people are arguing and debationg over a topic")
+sleep(1)
+print("This was when the public voted for the king to be executed or not")
+sleep(1)
+print("They alll had a vote as they beleived the king was doing a terrible act")
+sleep(1)
+print("He was trying to work with frances enemies to stop the revolution!")
+sleep(1)
+print("*They look at the final vote*")
+sleep(1)
+print("He is guilty and punished to an execution!!!")
+sleep(1)
+print("*You watch the them use the guillotine")
+sleep(1)
+print("*people are happy after seeing his head chopped off")
+sleep(1)
+print("*You get back into the car a slight bit traumatised")
+
+
+
